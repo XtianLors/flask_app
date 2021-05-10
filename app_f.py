3c9fa163d07e25484f2cef683a47252bba6acf99
XtianLoRs <crisdan_lori@outlook.com>
2021-05-10 14:18:01 -0500

    Dynamic added

--- app_f.py
+++ app_f.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	user_agent = request.headers.get('User-Agent')
-	return '<p>Your Browser is {}'.format(user_agent)
+	return '<h1>Hello there!</h1>'+'<p>Your Browser is {}'.format(user_agent)
+
+@app.route('/user/<username>'):
+def user(username):
+	return '<h1>Hello, {}!'.format(username)
